e4f53f4513262f93062691240317a6959f309065
iaouvaeiru <leonkind@gmail.com>
2023-05-09 23:59:57 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `yowoyuwul`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
