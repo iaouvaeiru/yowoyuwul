595277d46747958246ab475c8610385addb8d531
iaouvaeiru <leonkind@gmail.com>
2023-05-12 03:34:58 -0500

    more

--- src/pages/index.js
+++ src/pages/index.js
@@ -127,7 +127,7 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Congratulations WOW well done
+        Congratulations WOW well done more text
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
       </h1>
