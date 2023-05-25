939ac49124f76add10ac743b58e62bd6d79b9bb4
iaouvaeiru <leonkind@gmail.com>
2023-05-24 21:59:10 -0500

    content for faq page

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -5,7 +5,8 @@ const Faq = () => {
     return (
         <Layout>
             <div>
-            
+                <img src='https://i.imgur.com/6Olxq0m.jpg' alt='must be good then?' />
+                image source: https://www.reddit.com/r/RocketLeague/comments/g804a3/silver_league_for_life/
             </div>
         </Layout>
     );
