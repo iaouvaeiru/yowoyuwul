9e34311b98ad545ae5b95e77eb2f0907ed2a03c1
iaouvaeiru <leonkind@gmail.com>
2023-05-25 23:55:27 -0500

    some faq css

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Layout from "../components/Layout"
 
+const imgLayout = {
+    height: "auto",
+    maxWidth: '60%',
+    display: "block",
+    marginLeft: "auto",
+    marginRight: 'auto'
+}
+
 const Faq = () => {
     return (
         <Layout>
             <div>
-                <img src='https://i.imgur.com/6Olxq0m.jpg' alt='must be good then?' />
+                <img src='https://i.imgur.com/6Olxq0m.jpg' alt='must be good then?'  style={imgLayout}/>
+                <br/>
                 image source: https://www.reddit.com/r/RocketLeague/comments/g804a3/silver_league_for_life/
             </div>
         </Layout>
