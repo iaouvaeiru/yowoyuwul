0238f67263c96a01c3bf31e8d23174e374ff3001
iaouvaeiru <leonkind@gmail.com>
2023-05-30 23:45:01 -0500

    working on presets js file

--- src/pages/meet-the-presets.js
+++ src/pages/meet-the-presets.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import Layout from "../components/Layout"
 
+const yajjiiPresetArray = []
+
+const yajjiiPresetGenerator = () => {
+    return (yajjiiPresetArray.map(presetObj => {
+        return (
+            <div>
+
+            </div>
+        )
+    }))
+}
+
+const roughBuddyPresetArray = []
+
+const roughBuddyPresetGenerator = () => {
+    return (roughBuddyPresetArray.map(presetObj => {
+        return (
+            <div>
+                
+            </div>
+        )
+    }))
+}
+
 const MeetThePresets = () => {
     return (
         <Layout>
