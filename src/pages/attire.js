a5702f364aeb0d2cf59480d2d799ce4d8d255650
iaouvaeiru <leonkind@gmail.com>
2023-05-31 00:00:07 -0500

    more preset work and cleanup of other files

--- src/pages/attire.js
+++ src/pages/attire.js
@@ -2,12 +2,9 @@ import React from 'react';
 import Layout from "../components/Layout"
 import boomMaestro from "../images/boommaestro.png"
 import boomFennec from "../images/boomfennec.png"
-import yajDominus from "../images/yajdominus.png"
-import yajFennec from "../images/yajfennec.png"
-import yajOctane from "../images/yajoctane.png"
-import yajOctane2 from "../images/yajoctane2.png"
 
-const attireArray = [boomMaestro, boomFennec, yajDominus, yajFennec, yajOctane, yajOctane2]
+
+const attireArray = [boomMaestro, boomFennec]
 
 const attireRenderer = () => {
     return attireArray.map(image => {
--- src/pages/meet-the-presets.js
+++ src/pages/meet-the-presets.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import Layout from "../components/Layout"
+import yajDominus from "../images/yajdominus.png"
+import yajFennec from "../images/yajfennec.png"
+import yajOctane from "../images/yajoctane.png"
+import yajOctane2 from "../images/yajoctane2.png"
 
-const yajjiiPresetArray = []
+const yajjiiPresetArray = [
+    {
+        image: yajDominus,
+        name: "yajDominus"
+    },
+    {
+        image: yajFennec,
+        name: "yajFennec"
+    },
+    {
+        image: yajOctane,
+        name: "yajOctane"
+    },
+    {
+        image: yajOctane2,
+        name: "yajOctane2"
+    }
+]
 
 const yajjiiPresetGenerator = () => {
     return (yajjiiPresetArray.map(presetObj => {
@@ -19,7 +40,7 @@ const roughBuddyPresetGenerator = () => {
     return (roughBuddyPresetArray.map(presetObj => {
         return (
             <div>
-                
+
             </div>
         )
     }))
