a55622f3653d9234e3d42d330b528f933752a414
iaouvaeiru <leonkind@gmail.com>
2023-06-13 20:51:24 -0500

    added some text

--- src/pages/our-story.js
+++ src/pages/our-story.js
@@ -5,7 +5,12 @@ const OurStory = () => {
     return (
         <Layout>
             <div>
-                We're just normal men, just innocent men
+                <p>
+                    We're just normal men, just innocent men.
+                </p>
+                <p>
+                    Joe used to be good at rocket league, then he stopped playing for a bit because he wanted to be a pro a rainbow 6 siege or overwatch or something and now hes bad like the rest of us.
+                </p>
             </div>
         </Layout>
     );
