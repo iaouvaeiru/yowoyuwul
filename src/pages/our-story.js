22b9da5e56e54acc6098041748d10da35274129e
iaouvaeiru <leonkind@gmail.com>
2023-07-07 13:33:12 -0500

    added stuff

--- src/pages/our-story.js
+++ src/pages/our-story.js
@@ -11,6 +11,9 @@ const OurStory = () => {
                 <p>
                     Joe used to be good at rocket league, then he stopped playing for a bit because he wanted to be a pro a rainbow 6 siege or overwatch or something and now hes bad like the rest of us.
                 </p>
+                <p>
+                    From League of Legends to Rocket League, the search for a good game continues.
+                </p>
             </div>
         </Layout>
     );
